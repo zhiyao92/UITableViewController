4597d7d05b52cdcaca6abfeda1909094eb6ece80
Kelvin <kelvintan@MEDIA15-127117.ad.byuh.edu>
2017-03-15 14:09:06 -1000

    First Commit

---
+++ TabbleView/TableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TableViewController.swift
+//  TabbleView
+//
+//  Created by Kelvin on 3/15/17.
+//  Copyright © 2017 Kelvin. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var data = ["Apple", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry",
+                  "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit",
+                  "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango",
+                  "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach",
+                  "Pear", "Pineapple", "Raspberry", "Strawberry"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    // MARK: - Table view data source
+    
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
+
+
+}
